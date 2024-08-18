8db7038c5d2680fe01a2cb480fc8878dfb4e59e4
Tony Le <hoai.nam.le@outlook.com>
2024-08-18 17:46:14 -0400

    test

--- run_tests.sh
+++ run_tests.sh
@@ -7,11 +7,13 @@ emulator -avd Nexus_6 -no-snapshot -no-boot-anim -no-audio -no-window &
 adb wait-for-device
 adb shell input keyevent 82
 
-# Wait for the emulator to boot
+# Wait for the emulator to fully boot
 echo "Waiting for the emulator to fully boot..."
 sleep 60  # Adjust this if necessary
 
+# Change to the project directory
+cd news_app
+
 # Run the integration tests
 echo "Running integration tests..."
-cd news_app
-flutter drive --driver=test_driver/main_test.dart --target=integration_test/app_test.dart --verbose
+flutter drive --driver=test_driver/integration_test.dart --target=integration_test/app_test.dart --verbose
